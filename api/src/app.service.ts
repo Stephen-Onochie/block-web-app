a8cd942f486b72f19aee25b35be97b89dfdde3d5
Stephen Onochie <stephenconochie@gmail.com>
2025-11-29 18:21:12 -0500

    Restarted Repo for clear api and app code separation

--- api/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-// api/src/app.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello() {
-    return { message: 'Block API is running' };
-  }
-}
