24ca7548d87a5414d6afaca26f0a125d9c64e900
Stephen Onochie <stephenconochie@gmail.com>
2025-12-04 10:27:56 -0500

    Started work on Backend API
    - Added basic code for api endpoints
    - need to ensure I include all critical GET requests so front end can work well

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,1 +1,23 @@
 //main backend code
+
+const app = require('@nestjs/platform-express');
+
+const PORT = 8080;
+
+//frontend api request for specific property data
+app.get('/property-data', (req: any,res: any) => {
+    res.send(200).send({
+        //property data from ATTOM API
+    })
+});
+
+
+//api request to create cluster
+
+//api request to get routes
+
+
+app.listen(
+    PORT,
+    () => console.log(`Server running on port ${PORT}`)
+    );
